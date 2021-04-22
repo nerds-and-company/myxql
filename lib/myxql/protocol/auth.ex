c15cfee76b0d5416ccd6acef123cd9102e4bc2e8
David Kumru <davidkumru@gmail.com>
2021-04-22 12:29:03 +0200

    default auth response

--- lib/myxql/protocol/auth.ex
+++ lib/myxql/protocol/auth.ex
@@ -44,6 +44,9 @@ defmodule MyXQL.Protocol.Auth do
 
       auth_plugin_name == "caching_sha2_password" ->
         sha256_password(config.password, initial_auth_plugin_data)
+
+      true ->
+        ""
     end
   end
 
